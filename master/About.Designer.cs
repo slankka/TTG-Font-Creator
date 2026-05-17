744ee267af24641386af71d9d106b257377d7693
slankka <cor_twi@outlook.com>
2026-05-17 19:35:07 +0800

    docs(About): 更新为 TTG Font Builder 名称与功能描述

--- master/About.Designer.cs
+++ master/About.Designer.cs
@@ -68,7 +68,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MaximizeBox = false;
             this.Name = "About";
-            this.Text = "About";
+            this.Text = "About TTG Font Builder";
             this.ResumeLayout(false);
 
         }
